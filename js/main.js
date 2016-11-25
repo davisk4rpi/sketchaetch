ff6f3884f44ac49d21f23418b9c3dcae93248589
Kieran Davis <davisk4rpi@gmail.com>
2016-11-25 14:08:35 -0800

    add js to color divs after hover

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ $(document).ready(function(){
 		$('.container').append('<div class="row"></div>');
 	};
 	for (var j = 0; j < 16; j++){
-			$('.row').append('<div class="col"></div>');
+			$('.row').append('<div class="col blank"></div>');
 	};
+	$('div.blank').mouseenter(function(){
+		var x = Math.floor(Math.random()*256);
+		var y = Math.floor(Math.random()*256);
+		var z = Math.floor(Math.random()*256);
+		$(this).css("background-color", "rgb(" + x + ", " + y + ", " + z + ")");
+		$(this).removeClass("blank");
+		$(this).addClass("color");
+	});
 
+	
 });
