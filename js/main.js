25d54fbe6cc48472e324aac9fdaf0ec600d89c52
Kieran Davis <davisk4rpi@gmail.com>
2016-11-25 15:46:07 -0800

    each successive pass over a box darkens it by 10% until it is black

--- js/main.js
+++ js/main.js
@@ -3,16 +3,66 @@ $(document).ready(function(){
 		$('.container').append('<div class="row"></div>');
 	};
 	for (var j = 0; j < 16; j++){
-			$('.row').append('<div class="col blank"></div>');
+			$('.row').append('<div class="col blank"></div></div>');
+
 	};
-	$('div.blank').mouseenter(function(){
-		var x = Math.floor(Math.random()*256);
-		var y = Math.floor(Math.random()*256);
-		var z = Math.floor(Math.random()*256);
-		$(this).css("background-color", "rgb(" + x + ", " + y + ", " + z + ")");
-		$(this).removeClass("blank");
-		$(this).addClass("color");
+	$('div.col').mouseenter(function(){
+		if ($(this).hasClass('blank')){
+			var x = Math.floor(Math.random()*256);
+			var y = Math.floor(Math.random()*256);
+			var z = Math.floor(Math.random()*256);
+			$(this).append('<div class="overlay"></div>');
+			$(this).removeClass('blank');
+			$(this).css("background-color", "rgb(" + x + ", " + y + ", " + z + ")");
+		}
+		else{
+			if ($("div", this).hasClass('overlay')){
+				$("div", this).removeClass('overlay');
+				$("div", this).addClass('overlay1');
+			}
+			else if ($("div", this).hasClass('overlay1')){
+				$("div", this).removeClass('overlay1');
+				$("div", this).addClass('overlay2');
+			}
+			else if ($("div", this).hasClass('overlay2')){
+				$("div", this).removeClass('overlay2');
+				$("div", this).addClass('overlay3');
+			}
+			else if ($("div", this).hasClass('overlay3')){
+				$("div", this).removeClass('overlay3');
+				$("div", this).addClass('overlay4');
+			}
+			else if ($("div", this).hasClass('overlay4')){
+				$("div", this).removeClass('overlay4');
+				$("div", this).addClass('overlay5');
+			}
+			else if ($("div", this).hasClass('overlay5')){
+				$("div", this).removeClass('overlay5');
+				$("div", this).addClass('overlay6');
+			}
+			else if ($("div", this).hasClass('overlay6')){
+				$("div", this).removeClass('overlay6');
+				$("div", this).addClass('overlay7');
+			}
+			else if ($("div", this).hasClass('overlay7')){
+				$("div", this).removeClass('overlay7');
+				$("div", this).addClass('overlay8');
+			}
+			else if ($("div", this).hasClass('overlay8')){
+				$("div", this).removeClass('overlay8');
+				$("div", this).addClass('overlay9');
+			}
+			else if ($("div", this).hasClass('overlay9')){
+				$("div", this).removeClass('overlay9');
+				$("div", this).addClass('overlay10');
+			}
+			else{}
+		}
 	});
 
-	
+	$('div.col').mouseleave(function(){
+		$(this).addClass('color');
+	});
+
+
 });
