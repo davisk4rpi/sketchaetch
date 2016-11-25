6de42929ddb4e358a1f35f335813ee579479b93b
Kieran Davis <davisk4rpi@gmail.com>
2016-11-25 13:24:10 -0800

    create directory structure and basic div structure with js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	for (var i = 0; i < 16; i++){
+		$('.container').append('<div class="row"></div>');
+	};
+	for (var j = 0; j < 16; j++){
+			$('.row').append('<div class="col"></div>');
+	};
+
+});
